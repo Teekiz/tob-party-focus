861a5d592534dbbc29fe5356019d5e26d70dcca3
Callum <callumdavidrossiter@gmail.com>
2024-08-03 20:22:36 +0100

    Added check to find players in party (still need to add a check if no players are available).

--- src/main/java/com/tobpartyfocus/TobPartyFocusPlugin.java
+++ src/main/java/com/tobpartyfocus/TobPartyFocusPlugin.java
@@ -1,13 +1,18 @@
 package com.tobpartyfocus;
 
 import com.google.inject.Provides;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.Player;
 import net.runelite.api.VarPlayer;
 import net.runelite.api.Varbits;
+import net.runelite.api.events.ChatMessage;
+import net.runelite.api.events.CommandExecuted;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.VarbitChanged;
 import net.runelite.client.config.ConfigManager;
@@ -23,9 +28,10 @@ public class TobPartyFocusPlugin extends Plugin
 {
 	@Inject
 	private Client client;
-
 	@Inject
 	private TobPartyFocusConfig config;
+
+	private int tobPartyWidgetID = 1835020; //Teekiz<br>-
 	private boolean isInParty = false;
 
 	@Override
@@ -40,15 +46,18 @@ public class TobPartyFocusPlugin extends Plugin
 		log.info("Example stopped!");
 	}
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
+	//check if player is a party - done
+	//check to see if the player is within a certain boundry
+	//hide players and overhead text
 
+	//party size?
+	//key set?
+	public List<String> getPartyMembers()
+	{
+		String playersNamesInParty = client.getWidget(tobPartyWidgetID).getText();
+		playersNamesInParty = playersNamesInParty.replace("-", "");
+		return List.of(playersNamesInParty.split("<br>"));
+	}
 	@Subscribe
 	public void onVarbitChanged(VarbitChanged varbitChanged)
 	{
@@ -57,6 +66,27 @@ public class TobPartyFocusPlugin extends Plugin
 			isInParty = varbitChanged.getValue() == 1;
 		}
 	}
+
+	//overhead text could be getting all text and setting it to ""
+	@Subscribe
+	public void onChatMessage(ChatMessage chatMessage)
+	{
+		if (!chatMessage.getName().equalsIgnoreCase("partyMember"))
+		{
+			//filter
+		}
+	}
+
+	@Subscribe
+	public void onCommandExecuted(CommandExecuted commandExecuted)
+	{
+		//::tobtest
+		if (commandExecuted.getCommand().equalsIgnoreCase("tobtest"))
+		{
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", getPartyMembers().get(0), null);
+		}
+	}
+
 
 	@Provides
 	TobPartyFocusConfig provideConfig(ConfigManager configManager)
